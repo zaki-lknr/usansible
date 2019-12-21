3e179cdf9c4ee0bbfbfd8f083abc0eb35a277110
zaki <zaki.hmkc@gmail.com>
2019-12-21 11:58:53 +0900

    add: pluginsのソースのリンクに対応

--- src/scripts.js
+++ src/scripts.js
@@ -13,7 +13,7 @@ li.innerHTML += ' / <a href="' + baseurl + '">View Source</a>'
  */
 function getBranchName() {
     // target versionをURLから取出し
-    let v = document.URL.match(/docs\.ansible\.com\/ansible\/(.*?)\/modules/);
+    let v = document.URL.match(/docs\.ansible\.com\/ansible\/(.*?)\/(?:modules|plugins)/);
     console.log(v[1]);
     let ver;
     switch (v[1]) {
@@ -30,26 +30,34 @@ function getBranchName() {
     return ver;
 }
 
-function getGitHubUrl(version) {
-    // [Edit on GitHub]の位置を取り出し
-    // <a href="https://github.com/ansible/ansible/edit/devel/lib/ansible/modules/system/service.py?description=%23%23%23%23%23%20SUMMARY%0A%3C!---%20Your%20description%20here%20--%3E%0A%0A%0A%23%23%23%23%23%20ISSUE%20TYPE%0A-%20Docs%20Pull%20Request%0A%0A%2Blabel:%20docsite_pr" class="fa fa-github"> Edit on GitHub</a>
-    //let editlink = document.getElementsByClassName("fa-github")[0].getAttribute('href')
-    let editlinkobj = document.getElementsByClassName("fa-github")[0];
-    if (!editlinkobj) {
-        // version2.5は書式違う
-        editlinkobj = document.getElementsByClassName("icon-github")[0];
-    }
-    if (!editlinkobj) {
-        // console.log("*** not found ***")
-        exit;
-    }
-    let editlink = editlinkobj.getAttribute("href");
+/**
+ * GitHubのソースのURLを取得する
+ */
+function getGitHubUrl(branch) {
+    // ページ上部の[Edit on GitHub]の位置からソースのURL取得、モジュールは良いがプラグインが同じやり方だとリンク取得できない。
+    // よってページ下部の[!]Hintの項目内にある"edit this document"のリンクから取得する。
+    let editlink;
+    let external_links = document.getElementsByClassName("external");
+    if (external_links) {
+        // console.log(external_links);
+        // HTMLCollectionで配列メソッドを使う | ハックノート
+        // https://hacknote.jp/archives/21892/
+        let l = Array.prototype.slice.call(external_links).filter(o => o.getAttribute("href").match(/^https:\/\/github.*\/devel\/lib\/.*\.py\??/))
+        // foobar.pyのあとに"?..."がないリンクもあるので注意
+        //https://docs.ansible.com/ansible/2.7/plugins/callback/yaml.html
+        // console.log("link: " + l);
 
-    // [Edit on GitHub]のリンクからモジュールのソースを取り出し
-    let link = editlink.match(/(https:\/\/github\.com\/ansible\/ansible\/edit\/devel\/lib\/ansible\/)(modules\/.*?)\?/)
-    //console.log("link: " + link[2])
-    let baseurl = link[1].replace(/edit\/devel/, "blob/" + ver)+ link[2];
-    console.log("github url: " + baseurl);
+        if (l.length) {
+            editlink = l[0].getAttribute("href");
+        }
+    }
+    if (!editlink) {
+        console.log("=== link not found ===");
+        return null;
+    }
+    console.log(editlink)
 
-    return baseurl;
+    let link = editlink.match(/(https:\/\/github\.com\/ansible\/ansible\/edit\/devel\/lib\/ansible\/(?:modules|plugins)\/.*?\.py)\??/)
+    // console.log("link: " + link[1]);
+    return link[1].replace(/edit\/devel/, "blob/" + branch);
 }
