b86099fb7dff2a041e5b39791be4a97cad4d1884
zaki <zaki.hmkc@gmail.com>
2022-07-08 23:06:48 +0900

    update: manifest v3で使用不能になったtabs.getSelected()からtabs.query()への変更 #17

--- src/background.js
+++ src/background.js
@@ -1,10 +1,12 @@
 chrome.omnibox.onInputEntered.addListener(
     function (text, disposition) {
-        console.log("text: " + text);
+        // console.log("text: " + text);
         let url = "https://docs.ansible.com/ansible/latest/search.html?q=" + text + '#stq=' + text + '&stp=1';
 
-        chrome.tabs.getSelected(null, function (tab) {
-            chrome.tabs.update(tab.id, { url: url });
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
+            // console.log("url:: " + url);
+            // console.log("tabs: " + tabs[0].id);
+            chrome.tabs.update(tabs[0].id, { url: url });
         });
     }
 )
