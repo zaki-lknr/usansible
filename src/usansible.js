49a3587924542a1a2c9a5bffff2dfde04fd71cd3
zaki-lknr <zaki.hmkc+github@gmail.com>
2026-07-18 11:01:39 +0900

    update: core以外のリポジトリはデフォルトブランチに決まりはないためHEADで代替 #15

--- src/usansible.js
+++ src/usansible.js
@@ -160,12 +160,12 @@ const getCollectionUrl = (branch) => {
                 // 例えばansible.posix.firewalld module
                 // https://docs.ansible.com/ansible/latest/collections/ansible/posix/firewalld_module.html
                 // https://github.com/ansible-collections/ansible.posix/blob/main/plugins/modules/firewalld.py
-                return link + '/blob/main/plugins/modules/' + m[3] + '.py';
+                return link + '/blob/HEAD/plugins/modules/' + m[3] + '.py';
                 // "module" -> "modules" (sが増えてる)
             }
             else {
                 // module以外はパス名そのまま
-                return link + '/blob/main/plugins/'+ m[4] +'/' + m[3] + '.py';
+                return link + '/blob/HEAD/plugins/'+ m[4] +'/' + m[3] + '.py';
             }
         }
     }
